0317136e950171d277e030492f546c36b2b3b6d8
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2022-08-12 21:45:52 +0600

    result published

---
+++ assets/Home.d6bb2584.js
@@ -0,0 +1,12 @@
+import{r as d,o as e,c as s,a as t,b as c,w as r,u as i,d as l,e as o}from"./index.9fd20df5.js";const h={class:"bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white"},u={class:"container py-10 mx-auto text-center min-h-screen grid place-items-center"},m={class:"max-w-lg rounded-2xl mx-auto p-3 space-y-5"},p=t("h1",{class:"text-2xl"},"Potential buetian hunt",-1),x=t("h3",null,"22 Batch",-1),f={key:0,class:"px-3"},g=t("br",null,null,-1),b=t("h2",{class:"text-red-500 font-semibold text-lg"}," Marks Distribution ",-1),k=t("pre",null,`
+Physics 10
+Chemistry 10
+Math 10
+
+Per question mark 10
+Negative mark 2.5
+
+Total Question: 30
+Total Marks: 300
+Exam Duration: 1 hour
+                    `,-1),v={key:0,class:"pb-5"},y={key:0,class:"text-green-600 font-semibold text-lg"},w={class:"flex justify-center pb-3 gap-3"},N=o("Go to Exam"),B={key:1,class:"space-y-4"},C=t("p",{class:"text-center text-xl"},[o("\u0985\u0982\u09B6\u0997\u09CD\u09B0\u09B9\u09A3 \u0995\u09B0\u09BE\u09B0 \u09B8\u09AE\u09DF \u09B6\u09C7\u09B7\u0964 "),t("br"),o(" \u09B0\u09C7\u099C\u09BE\u09B2\u09CD\u099F \u099C\u09BE\u09A8\u09A4\u09C7 \u09A8\u09BF\u099A\u09C7\u09B0 \u09AC\u09BE\u099F\u09A8\u09C7 \u0995\u09CD\u09B2\u09BF\u0995 \u0995\u09B0\u09CB")],-1),M=t("div",{class:"flex items-center justify-center gap-2 mt-3 md:gap-4 flex-wrap"},[t("a",{href:"https://drive.google.com/file/d/1QFIEoS7fdzWfxNslmwcNweeTMHqGP4BO/view",target:"_blank",class:"btn btn-success"},"Batch 22 Result"),t("a",{href:"https://drive.google.com/file/d/1CM_9W6yu19G17P_Ja4fXnFYi7hQwz4cG/view",target:"_blank",class:"btn btn-success"},"Batch 23 Result")],-1),P=[C,M],D={__name:"Home",setup(T){const _=16602408e5<Date.now();let a=JSON.parse(localStorage.getItem("buethunt22exam"))||null;return(E,G)=>{const n=d("router-link");return e(),s("div",h,[t("div",u,[t("div",m,[c(n,{to:"/",as:"div",class:"text-center uppercase text-white"},{default:r(()=>[p,x]),_:1}),_?(e(),s("div",B,P)):(e(),s("div",f,[g,b,k,i(a)?(e(),s("div",v,[i(a).score>0?(e(),s("p",y," Thanks for participation. ")):l("",!0)])):l("",!0),t("div",w,[c(n,{to:"/exam",class:"btn"},{default:r(()=>[N]),_:1})])]))])])])}}};export{D as default};
